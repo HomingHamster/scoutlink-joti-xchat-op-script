038e3adeac0482a24d8ec9e715e6c2dac245b46d
Felix Farquharson <felix.farquharson@googlemail.com>
2012-10-19 03:13:37 +0100

    added a method of noticing a channel you join added an enable method

--- JOTIop.py
+++ JOTIop.py
@@ -25,6 +25,7 @@ print "\0034",__module_name__, __module_version__,"has been loaded\003"
 #warn people automatically about rudeness.
 #record people who need a rude kick (nicks red?)
 #warn if channel too busy..
+#maintain a list of dangerous nicks by people who have been banned in the past
 
 #add the following options to the dropdown menu on nickname rightclick.
 #   warn/kick idle
@@ -39,19 +40,54 @@ class Joti:
         self.is_enabled = False
         self.channels = set()
 
+    def dispatch(self, word, word_eol, userdata):
+        print word
+        return xchat.EAT_ALL
+
+    def on_text(self, word, word_eol, userdata):
+        print word, word_eol, userdata
+        dest = xchat.get_context().get_info("channel")
+        
+    #this function handles when a user joins any channel
+    #that you are in while the script is active.
+    def on_join(self, word, word_eol, userdata):
+        #add user to list of people in the channel
+        print word, word_eol, userdata
+
+    #this function handles when a user parts any channel
+    #that you are in while the script is active.
+    def on_part(self, word, word_eol, userdata):
+        #remove user from list of users in channel
+        print word, word_eol, userdata
+
+    def on_channel_join(self, word, word_eol, userdata):
+        channel_temp = Channel("k2") # WAS |hERE lasdkjcnksdjncksjhfkjnfkejrnkjfnekjrnfkejnrfkjenkrfjnekjfrn
+        print channel_temp
+        self.channels.add(channel_temp)
+        return xchat.EAT_NONE
+
     def enable(self):
         self.is_enabled = True
         #read in config
-        config = ConfigParser.RawConfigParser()
-        config.read('config.conf')
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read('config.conf')
         #set menus
         self.setup_nick_menu()
         #create list of channels
+        try:
+            for channel in xchat.get_list("channel"):
+                dir(channel)
+        except KeyError:
+            pass
+        #watch for new channels joined
+        xchat.hook_command("join", self.on_channel_join)
+        #watch for channels left
+        xchat.hook_command("part", self.on_channel_part)
         #set hooks for joti/join/part/messages
         xchat.hook_command("joti", self.dispatch)
-        xchat.hook_print('Channel Message', on_text)
-        xchat.hook_print('Join', on_join)
-        xchat.hook_print('Part', on_part)
+        xchat.hook_print('Channel Message', self.on_text)
+        xchat.hook_print('Join', self.on_join)
+        xchat.hook_print('Part', self.on_part)
         #xchat.hook_print('Change Nick', on_change_nick)
         #start writing log
 
@@ -78,25 +114,11 @@ class Joti:
         xchat.command('MENU ADD \"$NICK/JOTI/Language/Speak\ English (Spanish)\"\
                                                 \"joti spanish-speak-english %s\"')
 
-    def dispatch(self, word, word_eol, userdata):
-        print word
-        return xchat.EAT_ALL
-
-    def on_text(self, word, word_eol, userdata):
-        print word, word_eol, userdata
-        dest = xchat.get_context().get_info("channel")
-        
-    def on_join(self, word, word_eol, userdata):
-        print word, word_eol, userdata
-
-    def on_part(self, word, word_eol, userdata):
-        print word, word_eol, userdata
-
 class Log:
     def __init__(self, logfile_name):
         self.logfile_name = logfile_name
 
-    def open_log(self, logfile_name)
+    def open_log(self, logfile_name):
         self.logfile = open(logfile, "a")
         self.logfile.write(str(datetime.utcnow) + " >> LOG OPENED\r\n")
 
@@ -106,15 +128,15 @@ class Log:
 '''
 For every channel joined there is a channel class,
 when enabled this will hold all of the users and 
-ban information
+ban information.
 '''
 class Channel:
     def __init__(self, name):
         self.name = name                #stores chanel name.
         self.users = set()              #stores a User object for everyone in the channel.
         self.banned_users = set()       #a set of CURRENTLY BANNED users.
-        self.auto_enabled               #enable automatic warnings.
-        #self.ops = Op[]
+        self.auto_enabled = False       #enable automatic warnings.
+        self.op_enabled = False         #enable any management
         #self.user_limit = user_limit
 
     '''
@@ -123,7 +145,7 @@ class Channel:
     the users variable. Creating the log file etc.
     '''
     def enable_op(self):
-        pass
+        self.op_enabled = True
 
     '''
     Function is called when you choose to stop being an operator
@@ -132,7 +154,8 @@ class Channel:
     session.
     '''
     def disable_op(self):
-        pass
+        self.op_enabled = False
+        #tidy up bans
 
     def check_idle(self):
         pass
@@ -154,17 +177,20 @@ class User:
     def warn_channel(self, message, channel):
         pass
 
-    def on_text(word, word_eol, userdata):
+    def on_text(self, word, word_eol, userdata):
         print word, word_eol, userdata
         dest = xchat.get_context().get_info("channel")
         
-    def on_join(word, word_eol, userdata):
+    def on_join(self, word, word_eol, userdata):
         print word, word_eol, userdata
 
-    def on_part(word, word_eol, userdata):
+    def on_part(self, word, word_eol, userdata):
         print word, word_eol, userdata
 
-    xchat.hook_print('Channel Message', on_text)
-    xchat.hook_print('Join', on_join)
-    xchat.hook_print('Part', on_part)
-    #xchat.hook_print('Change Nick', on_change_nick)
+joti1 = Joti()
+
+def enable(word, word_eol, userdata):
+    joti1.enable()
+    return xchat.EAT_ALL
+
+xchat.hook_command("joti-enable", enable)
